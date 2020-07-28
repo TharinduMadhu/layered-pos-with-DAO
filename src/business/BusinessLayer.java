1013089756420d21a514356a3a2460a6ecd570e4
LawrenceAsanka <asankalorance@gmail.com>
2020-07-29 03:43:33 +0530

    Add item to business layer

--- src/business/BusinessLayer.java
+++ src/business/BusinessLayer.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import dao.DataLayer;
 import util.CustomerTM;
+import util.ItemTM;
 
 public class BusinessLayer {
 
@@ -21,6 +22,22 @@ public class BusinessLayer {
 
   public static boolean updateCustomer(String name, String address, String customerId){
     return DataLayer.updateCustomer(new CustomerTM(customerId, name, address));
+  }
+
+  public static List<ItemTM> getAllItems(){
+    return DataLayer.getAllItems();
+  }
+
+  public static boolean saveItem(String code, String description, int qtyOnHand, double unitPrice){
+    return DataLayer.saveItem(new ItemTM(code, description, qtyOnHand, unitPrice));
+  }
+
+  public static boolean deleteItem(String itemCode){
+    return DataLayer.deleteItem(itemCode);
+  }
+
+  public static boolean updateItem(String description, int qtyOnHand, double unitPrice, String itemCode){
+    return DataLayer.updateItem(new ItemTM(itemCode, description, qtyOnHand, unitPrice));
   }
 
 }
